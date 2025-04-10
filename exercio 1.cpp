e323db6951135c18c88def2cbaf3f5142a3b81f7
taynasantossouza <tayna.santos.souza23062008@gmail.com>
2025-04-10 14:45:49 -0300

    Add files via upload

---
+++ exercio 1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+	int v1,v2,v3;
+	float media;
+	printf("informe o valor 1\n");
+	scanf("%d",&v1);
+   	printf("informe o valor 2\n");
+	scanf("%d",&v2);
+	printf("informe o valor 3\n");
+	scanf("%d",&v3);
+	media=(v1+v2+v3)/3;
+	printf("media =%f",media);
+}
